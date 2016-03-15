20a45cc39338d3c4b233eaa0e13707a269a8492d
cs2student <jgu@caltech.edu>
2016-03-14 19:47:00 -0700

    implemented depth 2 min - max

--- player.cpp
+++ player.cpp
@@ -41,6 +41,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
+    using namespace std;
     Side other;
     if (this_side == WHITE){
         other = BLACK;
@@ -48,21 +49,22 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
     else if (this_side == BLACK){
         other = WHITE;
     }
-    int the_move[2];
+    //heuristic only code
+    /*int the_move[2];
     int cost = -99 * 64;
     player_board.doMove(opponentsMove, other);
     if (not player_board.hasMoves(this_side)){
         return NULL;
     }
     else {
-        int heuristic = player_board.value(this_side);
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++){
                 Board * new_board = player_board.copy();
                 Move * our_move = new Move(i, j);
                 if (new_board->checkMove(our_move, this_side)){
                     new_board->doMove(our_move, this_side);
-                    if (new_board->value(this_side) - heuristic > cost){
+                    if (new_board->value(this_side) > cost){
+                        cost = new_board->value(this_side);
                         the_move[0] = i;
                         the_move[1] = j;
                     }
@@ -73,6 +75,56 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
         }
     }
     Move * new_move = new Move(the_move[0], the_move[1]);
+    player_board.doMove(new_move, this_side);
+    return new_move;*/
+
+    //heuristic + depth 2 min - max code
+    player_board.doMove(opponentsMove, other);
+    int next_move[2];
+    int max_of_min = -99 * 64;
+    if (not player_board.hasMoves(this_side)){
+        return NULL;
+    }
+    else {
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                Board * new_board = player_board.copy();
+                Move * our_move = new Move(i, j);
+                if (new_board->checkMove(our_move, this_side)){
+                    new_board->doMove(our_move, this_side);
+                    int min_value = 99 * 64;
+                    if (not new_board->hasMoves(other)){
+                        new_board->doMove(NULL, other);
+                        min_value = new_board->value(this_side);
+                    }
+                    else {
+                        for (int a = 0; a < 8; a++){
+                            for (int b = 0; b < 8; b++){
+                                Board * ply_2_board = new_board->copy();
+                                Move * their_move = new Move(a, b);
+                                if (ply_2_board->checkMove(their_move, other)){
+                                    ply_2_board->doMove(their_move, other);
+                                    if (ply_2_board->value(this_side) < min_value){
+                                        min_value = ply_2_board->value(this_side);
+                                    }
+                                }
+                                delete their_move;
+                                delete ply_2_board;
+                            }
+                        }       
+                    }
+                    if (min_value > max_of_min){
+                        max_of_min = min_value;
+                        next_move[0] = i;
+                        next_move[1] = j;
+                    }
+                }
+                delete our_move;
+                delete new_board;
+            }
+        }
+    }
+    Move * new_move = new Move(next_move[0], next_move[1]);
     player_board.doMove(new_move, this_side);
     return new_move;
 }
